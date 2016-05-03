5138f28f717ade4359eff31845ee791be29930dd
Craig Trudo <craig.trudo@walmart.com>
2016-05-03 11:31:33 -0600

    initial commit

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,2 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {ContactsApp} from './app/contacts-app';
+
+bootstrap(ContactsApp);
+
+let test = new ContactsApp();
+
+test.getContacts();
--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,1 +1,16 @@
 import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'contacts-app',
+    template: "Hello World!"
+})
+
+export class ContactsApp{
+    constructor(){
+        
+    }
+
+    getContacts(){
+        return "stink";
+    }
+}
