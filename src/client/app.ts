31f93429d7065fe37546aeb3d92647d337a244a6
Terry Osbon <terry.osbon@gmail.com>
2016-05-03 11:34:44 -0600

    (checkpoint): ex 2

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,6 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {ContactsApp} from './app/contacts-app';
 
-let a = new ContactsApp();
-a.greatTime();
 bootstrap(ContactsApp);
--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,15 +1,12 @@
 import {Component} from 'angular2/core';
+import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
 
 @Component({
     selector: 'contacts-app',
     styleUrls: ['app/contacts-app.css'],
-    template: '<h1>Hello World!</h1>'
+    directives: [ContactHeaderComponent],
+    template: '<contact-header-component></contact-header-component>'
 })
 export class ContactsApp {
     constructor() {}
-    
-    //by default this is public; must be explicitly set to private to hide
-    greatTime() {
-        return "hi";
-    }
 }
