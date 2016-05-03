852df5ead1f6ef77c3ec27be4118a8cf987dc574
Terry Osbon <terry.osbon@gmail.com>
2016-05-03 10:59:59 -0600

    (checkpoint): excercise 1

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,2 +1,5 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {ContactsApp} from './app/contacts-app';
+
+
+bootstrap(ContactsApp);
--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,1 +1,10 @@
 import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'contacts-app',
+    styleUrls: ['app/contacts-app.css'],
+    template: 'Hello World! Auto-change detection?'
+})
+export class ContactsApp {
+    constructor() {}
+}
