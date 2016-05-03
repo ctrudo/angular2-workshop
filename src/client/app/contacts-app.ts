2961d7220f21cf1fac3f3b6d6bd33635dadd7945
Craig Trudo <craig.trudo@walmart.com>
2016-05-03 13:42:14 -0600

    exercise 4

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -4,6 +4,7 @@ import {Component} from 'angular2/core';
 // 'no extension' systemjs determines this and default is '.js'
 import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
 import {Contact} from './models/contact';
+import {CONTACT_DATA} from './data/contact-data';
 
 @Component({
     selector: 'contacts-app',
@@ -12,26 +13,27 @@ import {Contact} from './models/contact';
     template: `
 <contact-header-component></contact-header-component>
     <ul class="collection">
-        <li class="collection-item avatar">
+        <li *ngFor="#contact of contacts"   class="collection-item avatar">
             <img [src]="contact.image" alt="" class="circle">
             <span class="title">{{contact.name}}</span>
         </li>
     </ul>`
 })
 export class ContactsApp {
-    contact:Contact = {
-        id: 7,
-        name: 'Diana Ellis',
-        email: '',
-        phone: '',
-        birthday: '',
-        website: '',
-        image: '/assets/images/7.jpg',
-        address: {
-            street: '6554 park lane',
-            zip: '43378',
-            city: 'Rush',
-            country: 'United States'
-        }
-    }
+    // contact:Contact = {
+    //     id: 7,
+    //     name: 'Diana Ellis',
+    //     email: '',
+    //     phone: '',
+    //     birthday: '',
+    //     website: '',
+    //     image: '/assets/images/7.jpg',
+    //     address: {
+    //         street: '6554 park lane',
+    //         zip: '43378',
+    //         city: 'Rush',
+    //         country: 'United States'
+    //     }
+    // }
+    contacts:Array<Contact> = CONTACT_DATA
 }
