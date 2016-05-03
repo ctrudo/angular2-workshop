1148897247058f60a542027b5897ec79268c9aef
Terry Osbon <terry.osbon@gmail.com>
2016-05-03 11:58:00 -0600

    adding notes

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,4 +1,7 @@
 import {Component} from 'angular2/core';
+//2 interesting points: '.' and 'no extension'
+// '.' current directory is required to explicitly state this
+// 'no extension' systemjs determines this and default is '.js'
 import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
 
 @Component({
