58cc016e7964475987d2d0002cc42d6ce6a15170
Craig Trudo <craig.trudo@walmart.com>
2016-05-03 13:15:02 -0600

    exercise 3

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -3,13 +3,35 @@ import {Component} from 'angular2/core';
 // '.' current directory is required to explicitly state this
 // 'no extension' systemjs determines this and default is '.js'
 import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
+import {Contact} from './models/contact';
 
 @Component({
     selector: 'contacts-app',
     styleUrls: ['app/contacts-app.css'],
     directives: [ContactHeaderComponent],
-    template: '<contact-header-component></contact-header-component>'
+    template: `
+<contact-header-component></contact-header-component>
+    <ul class="collection">
+        <li class="collection-item avatar">
+            <img [src]="contact.image" alt="" class="circle">
+            <span class="title">{{contact.name}}</span>
+        </li>
+    </ul>`
 })
 export class ContactsApp {
-    constructor() {}
+    contact:Contact = {
+        id: 7,
+        name: 'Diana Ellis',
+        email: '',
+        phone: '',
+        birthday: '',
+        website: '',
+        image: '/assets/images/7.jpg',
+        address: {
+            street: '6554 park lane',
+            zip: '43378',
+            city: 'Rush',
+            country: 'United States'
+        }
+    }
 }
